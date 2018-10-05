03e3a82d456cd19fab1b8e2b9fefb6076bdfd85c
Ankit <ankityuvasoft206@gmail.com>
2018-10-05 16:30:16 +0530

    comment

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,2 @@
-$redis = Redis.new(:host => 'https://chat-app-yuva.herokuapp.com/', :port => 8080, :driver => :hiredis)
+# $redis = Redis.new(:host => 'https://chat-app-yuva.herokuapp.com/', :port => 8080, :driver => :hiredis)
+$redis = Redis.new(url: ENV["REDIS_URL"])
